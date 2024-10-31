0894705d7be82fc3cf0b4b54edd4cc6803f15b48
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-10-31 09:05:09 +0200

    Add controller

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,11 +1,23 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.service.ProvisioningService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class ProvisioningController {
+    private final ProvisioningService provisioningService;
 
-    // TODO Implement controller method
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/provisioning/{macAddress}")
+    public String getProvisioning(@PathVariable String macAddress) {
+        return provisioningService.getProvisioningFile(macAddress);
+    }
 }
